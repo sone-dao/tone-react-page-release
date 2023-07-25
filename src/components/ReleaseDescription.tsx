d3821e9d3633a256eeb0516d8868d27709e9e99f
Lon Beshiri <lonbeshiri@gmail.com>
2023-07-25 18:16:21 -0400

    fix: style

--- src/components/ReleaseDescription.tsx
+++ src/components/ReleaseDescription.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState } from 'react'
+import { CSSProperties, useState } from 'react'
 import styles from '../ReleasePage.module.scss'
 
 interface IReleaseDescriptionProps {
@@ -13,12 +13,19 @@ export default function ReleaseDescription({
   const [isOpen, setOpen] = useState<boolean>(false)
   const overLength = description.length > 1500 ? true : false
 
+  const style: CSSProperties = isOpen
+    ? {
+        height: 'auto',
+        overflow: 'auto',
+      }
+    : {
+        height: '33%',
+        overflow: 'hidden',
+      }
+
   return (
     <>
-      <div
-        style={{ height: isOpen ? 'auto' : '33vh' }}
-        className={styles.releaseDescription}
-      >
+      <div style={style} className={styles.releaseDescription}>
         <p>{description}</p>
       </div>
       {overLength && (
